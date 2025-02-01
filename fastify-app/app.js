0b2fc8076650d6c920de735ec3f6e7eaaeff8883
vgita <22777783+vgita@users.noreply.github.com>
2025-02-01 14:36:59 +0200

    basic project structure

--- fastify-app/app.js
+++ fastify-app/app.js
@@ -1,30 +1,32 @@
-'use strict'
+'use strict';
 
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
-
-// Pass --options via CLI arguments in command to enable these options.
-const options = {}
+const path = require('path');
+const AutoLoad = require('@fastify/autoload');
 
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
+    fastify.register(AutoLoad, {
+        dir: path.join(__dirname, 'schemas'),
+        indexPattern: /^loader.js$/i
+    });
 
-  // Do not touch the following lines
+    fastify.register(AutoLoad, {
+        dir: path.join(__dirname, 'plugins'),
+        dirNameRoutePrefix: false,
+        ignorePattern: /.*.no-load\.js/,
+        // ignorePattern: /^((?!load\.js).)*$/,
+        indexPattern: /^no$/i,
+        options: { ...opts }
+    });
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    fastify.register(AutoLoad, {
+        dir: path.join(__dirname, 'routes'),
+        indexPattern: /.*routes(\.js|\.cjs)$/i,
+        ignorePattern: /.*\.js/,
+        autoHooksPattern: /.*hooks(\.js|\.cjs)$/i,
+        autoHooks: true,
+        cascadeHooks: true,
+        options: Object.assign({}, opts)
+    });
+};
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
-
-module.exports.options = options
+module.exports.options = require('./configs/server-options');
---
+++ fastify-app/plugins/swagger.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fp = require('fastify-plugin');
+const fastifySwagger = require('@fastify/swagger');
+const fastifySwaggerUi = require('@fastify/swagger-ui');
+const pkg = require('../package.json');
+
+module.exports = fp(
+    async function swaggerPlugin(fastify, opts) {
+        fastify.register(fastifySwagger, {
+            swagger: {
+                info: {
+                    title: 'Fastify app',
+                    description: 'Fastify Book examples',
+                    version: pkg.version
+                }
+            }
+        });
+        fastify.register(fastifySwaggerUi, {
+            routePrefix: '/docs',
+            exposeRoute: fastify.secrets.NODE_ENV !== 'production'
+        });
+    },
+    { dependencies: ['application-config'] }
+);
